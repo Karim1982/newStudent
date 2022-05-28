8f0d74e36f8d80172ddbd400714b73cded5b9568
kimo <kimo84@gmail.com>
2022-05-28 11:42:15 +0200

    firstapp

---
+++ back end/student/src/main/java/com/spring/student/student/model/Gender.java
@@ -0,0 +1,5 @@
+package com.spring.student.student.model;
+
+public enum Gender {
+    male,female;
+}
---
+++ back end/student/src/main/java/com/spring/student/student/model/student.java
@@ -0,0 +1,12 @@
+package com.spring.student.student.model;
+
+import javax.xml.crypto.Data;
+
+public class student {
+    private int id;
+    private String fullname;
+    private String phone;
+    private String age;
+    private Gender gender;
+    private Data dataUpdate;
+}
